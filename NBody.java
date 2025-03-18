5b507f0356d840ede42fe2d85ce976c8ad03cc19
lauren-e-gardner <laurenator1784@gmail.com>
2025-03-18 17:05:10 -0400

    Initial commit

---
+++ NBody.java
@@ -0,0 +1,135 @@
+public class NBody {
+    public static void main(String[] args) {
+
+        // Parse command-line arguments
+        double tau = Double.parseDouble(args[0]);
+        double changeTau = Double.parseDouble(args[1]);
+        double g = 6.67e-11;
+        double distance;
+
+        // Read universe from standard input
+        int bodies = StdIn.readInt();
+        double radius = StdIn.readDouble();
+        double[] px = new double[bodies];
+        double[] py = new double[bodies];
+        double[] vx = new double[bodies];
+        double[] vy = new double[bodies];
+        double[] mass = new double[bodies];
+        String[] picture = new String[bodies];
+
+        for (int i = 0; i < bodies; i++) {
+            px[i] = StdIn.readDouble();
+            py[i] = StdIn.readDouble();
+            vx[i] = StdIn.readDouble();
+            vy[i] = StdIn.readDouble();
+            mass[i] = StdIn.readDouble();
+            picture[i] = StdIn.readString();
+        }
+
+        // Initialize standard drawing
+        StdDraw.setXscale(-radius, radius);
+        StdDraw.setYscale(-radius, radius);
+        StdDraw.enableDoubleBuffering();
+
+        // Play music on standard audio
+        StdAudio.play("2001.wav");
+
+        // Simulate the universe - loop:
+        for (double j = 0.0; j < tau; j += changeTau) {
+            double[] fx = new double[bodies * (bodies - 1)];
+            double[] fy = new double[bodies * (bodies - 1)];
+
+            // Account for one body (no force)
+            if (bodies == 1) {
+                StdDraw.clear();
+                StdDraw.picture(0, 0, "starfield.jpg");
+                vx[0] = vx[0] + (0 / mass[0]) * changeTau;
+                vy[0] = vy[0] + (0 / mass[0]) * changeTau;
+                px[0] = px[0] + (vx[0] * changeTau);
+                py[0] = py[0] + (vy[0] * changeTau);
+
+                // Draw the universe.
+                StdDraw.picture(px[0], py[0], picture[0]);
+
+                StdDraw.show();
+                StdDraw.pause(20);
+            }
+            else {
+                // 3 5A. Calculate the net force on each body.
+                for (int i = 0; i < bodies; i++) {
+                    for (int d = 0; d < bodies; d++) {
+                        if (i != d) {
+                            double force;
+                            // distances
+                            double pxSquared = Math.pow(px[d], 2) - Math.pow(px[i], 2);
+                            double pySquared = Math.pow(py[d], 2) - Math.pow(py[i], 2);
+
+                            distance = pxSquared + pySquared;
+
+                            force = (g * mass[i] * mass[d]) / distance;
+
+                            if (distance < 0) {
+                                if (pxSquared < 0)
+                                    fx[i] += (force * (px[d] - px[i])) /
+                                            (Math.sqrt(Math.abs(distance)) * -1);
+                                else
+                                    fx[i] += (force * (px[d] - px[i])) /
+                                            (Math.sqrt(Math.abs(distance)));
+                                if (pySquared < 0)
+                                    fy[i] += (force * (py[d] - py[i])) /
+                                            (Math.sqrt(Math.abs(distance)) * -1);
+                                else
+                                    fy[i] += (force * (py[d] - py[i])) /
+                                            (Math.sqrt(Math.abs(distance)));
+                                // accounts for abs value being taken ^^
+                            }
+
+                            else if (distance > 0) {
+                                if (pxSquared < 0)
+                                    fx[i] += (force * (px[d] - px[i])) /
+                                            (Math.sqrt(Math.abs(distance)));
+                                else
+                                    fx[i] += (force * (px[d] - px[i])) /
+                                            (Math.sqrt(Math.abs(distance)));
+                                if (pySquared < 0)
+                                    fy[i] += (force * (py[d] - py[i])) /
+                                            (Math.sqrt(Math.abs(distance)));
+                                else
+                                    fy[i] += (force * (py[d] - py[i])) /
+                                            (Math.sqrt(Math.abs(distance)));
+                                // accounts for abs value being taken ^^
+                            }
+                        }
+                    }
+                }
+
+
+                // Update the velocities and positions.
+                StdDraw.clear();
+                StdDraw.picture(0, 0, "starfield.jpg");
+                for (int k = 0; k < bodies; k++) {
+                    vx[k] = vx[k] + ((fx[k]) / mass[k]) * changeTau;
+                    vy[k] = vy[k] + ((fy[k]) / mass[k]) * changeTau;
+                    px[k] = px[k] + (vx[k] * changeTau);
+                    py[k] = py[k] + (vy[k] * changeTau);
+                }
+                // Draw the universe.
+                for (int m = 0; m < bodies; m++) {
+                    StdDraw.picture(px[m], py[m], picture[m]);
+                }
+                StdDraw.show();
+                StdDraw.pause(20);
+            }
+        }
+        // Print values
+        StdOut.printf("%d\n", bodies);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < bodies; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                          px[i], py[i], vx[i], vy[i], mass[i], picture[i]);
+        }
+    }
+
+}
+
+
---
+++ readme.txt
@@ -0,0 +1,66 @@
+This project was completed in Spring 2021 for COS126. The code can be run using the following commands:
+javac-introcs NBody.java
+java-introcs NBody 157788000.0 25000.0 < planets.txt
+
+/**********************************************************************/
+
+Programming Assignment: N-Body Simulation 
+
+
+PLEASE REMEMBER NOT TO INCLUDE YOUR NAME (OR LATER IN THE COURSE, YOUR
+PARTNER'S NAME) ANYWHERE IN THIS SUBMISSION.
+
+
+/**********************************************************************
+ * Approximate number of hours to complete this assignment            *
+ **********************************************************************/
+
+Number of hours: 4-5 hours
+
+
+
+/**********************************************************************
+ *  Did you receive help from classmates, past COS 126 students, or
+ *  anyone else? If so, please list their names.  ("A Sunday lab TA"
+ *  or "Office hours on Thursday" is ok if you don't know their name.)
+ **********************************************************************/
+
+Yes or no?  No  
+Names: 
+
+
+/**********************************************************************
+ *  Did you encounter any serious problems? If so, please describe.
+ **********************************************************************/
+
+Describe: I had some problems getting the sun to have any force exerted on it. 
+I also have a problem were my values are within a small margin of those in the 
+TigerFile submission tests. 
+
+
+
+/**********************************************************************
+ *  List any other comments here.                                     
+ **********************************************************************/
+
+
+
+/**********************************************************************
+ *  If you attempted Challenge #1, and submitted deluxe-universe.txt,
+ *  please explain your work and approach; in particular explain how
+ *  you figured out which values to put in the file to get your desired
+ *  result.
+ **********************************************************************/
+
+Describe:
+
+
+/**********************************************************************
+ *  If you attempted Challenge #2, and submitted DeluxeNBody.java,
+ *  please register an account on https://www.loom.com, record a 2-min.
+ *  video of yourself showing your universe in action, and explain what
+ *  you are doing.
+ **********************************************************************/
+
+Link to Loom video:
+
